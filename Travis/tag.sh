57e4da78f270c7ed9591f6da077eccb4da3fb4af
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-04-27 17:00:06 +0000

    remove unused file

--- Travis/tag.sh
+++
@@ -1,6 +0,0 @@
-git config --local user.name "traviscibot"
-git config --local user.email "traviscibot@travisci.org"
-git checkout master
-git tag -d latest
-git tag -a latest
-git push origin latest -f
