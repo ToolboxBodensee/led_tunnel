f0a19f6ee67368dffeae20662c55695a24627c94
L3D <l3d@see-base.de>
2019-05-01 22:41:22 +0200

    Optimize SSH

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir $HOME/.ssh
+echo "releases.tbbs.me ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5NslYj+wtriBuKIxJfDRm9E2hnlstWR8durQ6pKvVvP7wjcoXIyAxe41GvJ9SoEhSjF7oVlzlw+zAkzl5WOZ4=" >> $HOME/.ssh/known_hosts
 echo -e "$SSH_DEPLOY_KEY" > $HOME/.ssh/id_ed25519
 chmod 600 .ssh -R
 rsync d1_mini.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/ --progress
