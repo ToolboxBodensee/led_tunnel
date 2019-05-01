24be5e74c40898c813c9869ec0692541214dee7a
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-05-01 23:39:19 +0200

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ chmod 600 $HOME/.ssh/id_ed25519
 ls -la $HOME
 ls -la 
 rsync version web34@releases.tbbs.me:$SSH_DEPLOY_PATH/version --progress
-rsync $HOME/.pioenvs/nodemcu/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcu.bin --progress
-rsync $HOME/.pioenvs/d1_mini/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/d1_mini.bin --progress
-rsync $HOME/.pioenvs/nodemcuv2/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcuv2.bin --progress
+rsync .pioenvs/nodemcu/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcu.bin --progress
+rsync .pioenvs/d1_mini/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/d1_mini.bin --progress
+rsync .pioenvs/nodemcuv2/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcuv2.bin --progress
 rm -rf $HOME/.ssh/id_ed25519
