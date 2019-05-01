a160addf5fd46471ccca4cad0ebeac3833f1391d
L3D <l3d@see-base.de>
2019-05-01 22:41:22 +0200

    Optimize SSH for deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-mkdir $HOME/.ssh
+echo "releases.tbbs.me ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5NslYj+wtriBuKIxJfDRm9E2hnlstWR8durQ6pKvVvP7wjcoXIyAxe41GvJ9SoEhSjF7oVlzlw+zAkzl5WOZ4=" >> $HOME/.ssh/known_hosts
 echo -e "$SSH_DEPLOY_KEY" > $HOME/.ssh/id_ed25519
-chmod 600 .ssh -R
-rsync d1_mini.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/ --progress
-rsync nodemcu.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/ --progress
-rsync nodemcuv2.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/ --progress
-rsync version web34@releases.tbbs.me:$SSH_DEPLOY_PATH/ --progress
+chmod 600 $HOME/.ssh/id_ed25519
+ls -la $HOME
+ls -la 
+rsync version web34@releases.tbbs.me:$SSH_DEPLOY_PATH/version --progress
+rsync $HOME/.pioenvs/nodemcu/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcu.bin --progress
+rsync $HOME/.pioenvs/d1_mini/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/d1_mini.bin --progress
+rsync $HOME/.pioenvs/nodemcuv2/firmware.bin web34@releases.tbbs.me:$SSH_DEPLOY_PATH/nodemcuv2.bin --progress
 rm -rf $HOME/.ssh/id_ed25519
