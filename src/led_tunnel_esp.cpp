bb52cf5acce62068828bb45a9a0919281daf9c92
A. Binzxxxxxx <DEvil0000@users.noreply.github.com>
2019-09-22 02:13:40 +0200

    added arduino ota update

--- src/led_tunnel_esp.cpp
+++ src/led_tunnel_esp.cpp
@@ -6,6 +6,8 @@
 #include <avr/power.h>
 #endif
 
+#include <ArduinoOTA.h>
+
 #include "ota_update.h"
 #include "eeprom_settings.h"
 
@@ -42,6 +44,43 @@ void onFirmwareUpdateDone(unsigned int newVersion)
     saveConfig(cfg);
 }
 
+void setup_arduino_ota()
+{
+    ArduinoOTA.setHostname("LedTunnel");
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else { // U_SPIFFS
+            type = "filesystem";
+        }
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
+}
+
 void setup()
 {
     loadConfig(cfg);
@@ -61,6 +100,8 @@ void setup()
     Serial.begin(115200);
     Serial.println("");
     Serial.println("Start");
+
+    WiFi.hostname("LedTunnel");
     WiFi.mode(WIFI_STA);
     WiFi.begin(cfg.SSID, cfg.password);
     while (WiFi.status() != WL_CONNECTED)
@@ -70,6 +111,10 @@ void setup()
     }                                     // Verbunden
     Serial.println("");
     Serial.println("Verbunden");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    setup_arduino_ota();
 
     OTA_CONFIG ota_config = {
         .version = cfg.firmwareVer,
@@ -93,4 +138,5 @@ void setup()
 void loop()
 {
     led_animation_simple_larson_scanner();
+    ArduinoOTA.handle();
 }
